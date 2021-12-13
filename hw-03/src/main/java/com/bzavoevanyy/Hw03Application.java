6ecc9fe4ef766d80057d5dc6df531129d98def01
Bogdan Zavoevanyy <bzavoevanyy@gmail.com>
2021-12-13 17:03:26 +0500

    refactoring 4 was made

--- hw-03/src/main/java/com/bzavoevanyy/Hw03Application.java
+++ hw-03/src/main/java/com/bzavoevanyy/Hw03Application.java
@@ -1,9 +1,12 @@
 package com.bzavoevanyy;
 
+import com.bzavoevanyy.config.AppProps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProps.class)
 public class Hw03Application {
 
     public static void main(String[] args) {
--- hw-03/src/main/java/com/bzavoevanyy/config/AppProps.java
+++ hw-03/src/main/java/com/bzavoevanyy/config/AppProps.java
@@ -3,13 +3,8 @@ package com.bzavoevanyy.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 import java.util.Locale;
-
-@Configuration
-@EnableConfigurationProperties
 @ConfigurationProperties(prefix = "quiz")
 @Getter
 @Setter
